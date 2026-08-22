1e31f820edbdac323cee1e0421d46cf70cd5b450
Siddhesh-07 <siddheshgk07@gmail.com>
2026-08-22 21:31:03 +0530

    policy added

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -64,5 +64,10 @@ resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
 
 resource "aws_iam_role_policy_attachment" "eks_ecr_read_only_policy" {
   role       = aws_iam_role.eks_nodegroup_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonElasticContainerRegistryPublicReadOnly"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
+
+resource "aws_iam_role_policy_attachment" "ec2_read_only_policy" {
+  role       = aws_iam_role.eks_nodegroup_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess"
+} 
