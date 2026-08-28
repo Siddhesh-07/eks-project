7339621d307ebbfa43f4f88abe12d95491909cc1
Siddhesh-07 <siddheshgk07@gmail.com>
2026-08-28 13:32:57 +0530

    vpc configured

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,14 +1,3 @@
-data "aws_vpc" "project-vpc" {
-  id = var.vpc_id
-}
-
-data "aws_subnet" "private_1" {
-  id = var.subnet_pvt1
-}
-
-data "aws_subnet" "private_2" {
-  id = var.subnet_pvt2
-}
 
 data "aws_eks_cluster_auth" "project_eks" {
   name = aws_eks_cluster.project_eks.name
